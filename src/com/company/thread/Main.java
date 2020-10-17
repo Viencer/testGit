c6089558931f247c8eabd33f0d1af3372b0cc588
Avaro <yuriusis2001@gmail.com>
2020-10-17 18:43:21 +0300

    second commit

---
+++ src/com/company/thread/Main.java
@@ -0,0 +1,13 @@
+package com.company.thread;
+
+public class Main {
+    public static void main(String[] args) {
+boolean a = true;
+boolean b = false;
+boolean c = false;
+if (a & b | c){
+    System.out.println();
+    System.out.println(asd);
+}
+    }
+}
